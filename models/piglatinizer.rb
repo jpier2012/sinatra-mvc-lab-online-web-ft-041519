1dc1bf4614a9e991c88f0e0f11961a7648abc67a
James Pier <jpier2012@gmail.com>
2019-05-30 02:16:57 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,7 +8,7 @@ class PigLatinizer
       if word.match(/[aeiou]/)
         pig_words << "#{word}way"
       elsif word.match(/[^aeiou]/)
-        "#{word[0...word.index(/[aeiou]/)]}ay"
+        pig_words << "#{word[0...word.index(/[aeiou]/)]}ay"
         pig_words << "#{word[1..-1]}#{word[0]}ay"
       end
     end
