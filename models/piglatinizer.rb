6c9e45f35697ab7d09bbb97d26de03fcf58ec7cd
James Pier <jpier2012@gmail.com>
2019-05-30 01:58:50 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,8 +5,12 @@ class PigLatinizer
 
   def piglatinize
     words = string.split(' ')
-    word.each do |word|
-      "#{word[1..-1]}#{word[0]}ay"
+    words.each do |word|
+      if word[0].downcase.match(/[aeiou]/)
+        "#{word}way"
+      else  
+        "#{word[1..-1]}#{word[0]}ay"
+      end
     end
   end
 
